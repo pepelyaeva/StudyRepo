6eaf70e007151efeb07ec9b2f16de93191709f87
pepelyaeva <pepelyaevaks@gmail.com>
2016-10-27 20:43:45 +0600

    test find method

---
+++ lr_1.py
@@ -0,0 +1,10 @@
+﻿# avito.ru, результаты поиска по запросу "котята"
+
+from bs4 import BeautifulSoup
+import requests
+
+cont = requests.get('https://www.avito.ru/perm?q=котята').content
+soup = BeautifulSoup(cont)
+descr = soup.find('div', { "class" : "description" })
+print (descr)
+input()
