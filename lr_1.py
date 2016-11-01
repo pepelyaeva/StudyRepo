b0225cae1a180431fc639089902fdd916bb6fc98
pepelyaeva <pepelyaevaks@gmail.com>
2016-11-01 13:49:48 +0600

    add dict with title and section

--- lr_1.py
+++ lr_1.py
@@ -3,8 +3,11 @@
 from bs4 import BeautifulSoup
 import requests
 
+d = {}
+# title, section, address, time, link
 cont = requests.get('https://www.avito.ru/perm?q=котята').content
 soup = BeautifulSoup(cont)
-descr = soup.find('div', { "class" : "description" })
-print (descr)
+for i,item in enumerate(soup.find_all('div', { "class" : "description" })):
+	d['kotik' + str(i)] = {'title': item.h3.a.text.replace('\n',''), 'section': item.find('div', {'class':'data'}).p.text}
+print (d)
 input()
