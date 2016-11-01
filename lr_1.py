13e9f7f7ef7c63acbc97e2ea175d93de9a418215
pepelyaeva <pepelyaevaks@gmail.com>
2016-11-01 14:02:06 +0600

    add json encoding

--- lr_1.py
+++ lr_1.py
@@ -2,6 +2,7 @@
 
 from bs4 import BeautifulSoup
 import requests
+import json
 
 d = {}
 # title, section, address, time, link
@@ -9,5 +10,5 @@ cont = requests.get('https://www.avito.ru/perm?q=котята').content
 soup = BeautifulSoup(cont)
 for i,item in enumerate(soup.find_all('div', { "class" : "description" })):
 	d['kotik' + str(i)] = {'title': item.h3.a.text.replace('\n',''), 'section': item.find('div', {'class':'data'}).p.text}
-print (d)
+print(json.dumps(d, sort_keys=True, indent=4, ensure_ascii=False))
 input()
