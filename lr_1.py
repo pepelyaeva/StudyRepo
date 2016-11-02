d1f2de0bcf371aa8d74b484be7eb57d702642664
pepelyaeva <pepelyaevaks@gmail.com>
2016-11-02 22:31:16 +0600

    deleted comments

--- lr_1.py
+++ lr_1.py
@@ -17,11 +17,5 @@ for i,item in enumerate(soup.find_all('div', { "class" : "description" })):
 	except Exception:
 		address = 'none'
 	d['kotik' + str(i)] = {'title': title, 'section': data.contents[1].text, 'address':  address,'time': time}
-
-
-# нужно придумать что-то получше чем переделывать в массив
-# d['kotik' + str(i)] = {'title': title, 'section': data.p[0].text, 'address':  data.p[1].text, 'time': data.div}
-# d['kotik'] = {'title': title, 'section': data.contents[1].text, 'address':  data.contents[2].text, 'time': data.contents[3].div.div.text}
 print(json.dumps(d, sort_keys=False, indent=4, ensure_ascii=False))
-# print(d)
 input()
